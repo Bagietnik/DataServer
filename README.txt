62e74a061ee47502d89493ec4b11a6ed72d6457a
Bagietnik <Bagietnik>
2024-08-31 20:30:56 +0200

    Init

---
+++ README.txt
@@ -0,0 +1,41 @@
+Usage of mTLS
+
+Certs preparation: 
+
+Server:
+
+sudo apt-get install libssl-dev / OpenSSL installation
+
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes / priv key and the SSL cert
+
+gcc -o server server.c -lssl -lcrypto / compilation
+
+
+Client: 
+
+openssl genpkey -algorithm RSA -out client_key.pem / pub key
+
+openssl req -new -key client_key.pem -out client.csr / certificate signing reques
+
+openssl x509 -req -in client.csr -CA cert.pem -CAkey key.pem -CAcreateserial -out client_cert.pem -days 365 / self-signed CA. We will use the server's certificate as a certificate authority.
+
+openssl s_client -connect localhost:9999 -cert client_cert.pem -key client_key.pem / connection testing
+
+openssl x509 -in cert.pem -noout -text / cert info
+
+Fake client: 
+
+openssl genpkey -algorithm RSA -out fake_client_key.pem
+
+openssl req -new -key fake_client_key.pem -out fake_client.csr
+
+openssl genpkey -algorithm RSA -out fake_ca_key.pem
+
+openssl req -x509 -new -nodes -key fake_ca_key.pem -sha256 -days 365 -out fake_ca_cert.pem
+
+openssl x509 -req -in fake_client.csr -CA fake_ca_cert.pem -CAkey fake_ca_key.pem -CAcreateserial -out fake_client_cert.pem -days 365
+
+openssl s_client -connect localhost:9999 -cert fake_client_cert.pem -key fake_client_key.pem -CAfile cert.pem
+
+
+
---
+++ server.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#define PORT 9999
+#define BUFFER_SIZE 1024
+
+void handle_client(SSL *ssl) {
+    const char *json_message = "{\"status\": \"ok\", \"message\": \"Hello from secure server!\"}";
+
+    SSL_write(ssl, json_message, strlen(json_message));
+
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+}
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len;
+    pid_t child_pid;
+
+    SSL_load_error_strings();
+    OpenSSL_add_ssl_algorithms();
+
+    const SSL_METHOD *method = TLS_server_method();
+    SSL_CTX *ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (!SSL_CTX_load_verify_locations(ctx, "cert.pem", NULL)) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, NULL);
+    SSL_CTX_set_verify_depth(ctx, 1);
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, 5) < 0) {
+        perror("listen");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Secure server with client certificate verification listening on port %d\n", PORT);
+
+    while (1) {
+        client_addr_len = sizeof(client_addr);
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_socket < 0) {
+            perror("accept");
+            continue;
+        }
+
+        printf("Accepted connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+        child_pid = fork();
+        if (child_pid == 0) {
+            close(server_socket); 
+
+            SSL *ssl = SSL_new(ctx);
+            SSL_set_fd(ssl, client_socket);
+
+            if (SSL_accept(ssl) <= 0) {
+                ERR_print_errors_fp(stderr);
+            } else {
+                X509 *client_cert = SSL_get_peer_certificate(ssl);
+                if (client_cert != NULL) {
+                    printf("Client certificate:\n");
+                    X509_print_fp(stdout, client_cert);
+                    X509_free(client_cert);
+
+                    if (SSL_get_verify_result(ssl) == X509_V_OK) {
+                        handle_client(ssl);
+                    } else {
+                        printf("Client certificate verification failed.\n");
+                    }
+                } else {
+                    printf("No client certificate presented.\n");
+                }
+            }
+
+            close(client_socket);
+            exit(0);
+        } else if (child_pid > 0) {
+            close(client_socket);  
+        } else {
+            perror("fork");
+            close(client_socket);
+        }
+
+        while (waitpid(-1, NULL, WNOHANG) > 0);
+    }
+
+    close(server_socket);
+    SSL_CTX_free(ctx);
+    EVP_cleanup();
+
+    return 0;
+}
